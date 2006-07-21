7e698fac4e5294495b104b5aa54f29ca47f3d527
locasto <locasto>
2006-07-21 05:58:00 +0000

    boilerplate files, not important

---
+++ distro.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# package up a comtor distribution
+
+# blow away tmp
+# mkdir tmp
+# copy "CAPITAL" files to a tmp
+# copy code/ to tmp/, except for CVS/ dirs
+# move tmp to comtor-x.y.z
+# zip it (separate file)
+# tar it
+# gzip it
+
+echo $@
+
