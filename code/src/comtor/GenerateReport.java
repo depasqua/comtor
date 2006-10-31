a560e941305825672da3308a80b9fdb07585b6cd
brigand2 <brigand2>
2006-10-31 20:08:33 +0000

    Added $id:$ and comments.

---
+++ code/src/comtor/GenerateReport.java
@@ -0,0 +1,32 @@
+/***************************************************************************
+  *  Comment Mentor: A Comment Quality Assessment Tool
+  *  Copyright (C) 2005 Michael E. Locasto
+  *
+  *  This program is free software; you can redistribute it and/or modify
+  *  it under the terms of the GNU General Public License as published by
+  *  the Free Software Foundation; either version 2 of the License, or
+  *  (at your option) any later version.
+  *
+  *  This program is distributed in the hope that it will be useful, but
+  *  WITHOUT ANY WARRANTY; without even the implied warranty of 
+  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU 
+  *  General Public License for more details.
+  *
+  *  You should have received a copy of the GNU General Public License
+  *  along with this program; if not, write to the:
+  *  Free Software Foundation, Inc.
+  *  59 Temple Place, Suite 330 
+  *  Boston, MA  02111-1307  USA
+  *
+  * $Id: GenerateReport.java,v 1.1 2006-10-31 20:08:23 brigand2 Exp $
+  **************************************************************************/
+
+/**
+ * The <code>GenerateReport</code> class is a tool to
+ * generate a report from a vector containing property lists.
+ *
+ * @author Joe Brigandi
+ */
+public class GenerateReport{
+  
+}
