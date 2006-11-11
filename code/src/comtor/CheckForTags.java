a5e08d8acef959bc7d6412d88785b9dce69618d2
brigand2 <brigand2>
2006-11-11 03:48:29 +0000

    Added title, description, and date.

--- code/src/comtor/CheckForTags.java
+++ code/src/comtor/CheckForTags.java
@@ -18,7 +18,7 @@
   *  59 Temple Place, Suite 330 
   *  Boston, MA  02111-1307  USA
   *
-  * $Id: CheckForTags.java,v 1.9 2006-11-09 02:48:45 brigand2 Exp $
+  * $Id: CheckForTags.java,v 1.10 2006-11-11 03:48:29 brigand2 Exp $
   **************************************************************************/
 
 import com.sun.javadoc.*;
@@ -45,7 +45,7 @@ public class CheckForTags
   public Properties analyze(RootDoc rootDoc)
   {
 	prop.setProperty("title", "Check for Tags");
-	prop.setProperty("description", "This doclet checks for proper use of JavaDocs with returns, throw, and param tags.");
+	prop.setProperty("description", "This doclet checks for proper use of JavaDoc comments with returns, throw, and param tags.");
 	DateFormat dateFormat = new SimpleDateFormat("M/d/yy h:mm a");
 	java.util.Date date = new java.util.Date();
 	prop.setProperty("date", "" + dateFormat.format(date));
@@ -60,7 +60,8 @@ public class CheckForTags
       
       for(int j=0; j < methods.length; j++)
       {
-        prop.setProperty(i + "." + j, "method: " + methods[j].name());
+		String key = numberFormat(j);
+        prop.setProperty(i + "." + key, "method: " + methods[j].name());
         
 /////////////////return tags/////////////////
         String returnParam = "@return";
@@ -70,18 +71,18 @@ public class CheckForTags
         if(returntype=="void")
         {
           if(returnTags.length==0)
-            prop.setProperty(i + "." + j + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is void and there is no @return tag present in the comments.");
+            prop.setProperty(i + "." + key + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is void and there is no @return tag present in the comments.");
           else
-            prop.setProperty(i + "." + j + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is void but an @return tag is present in the comments. There should be no @return tag since the declared return type is void.");
+            prop.setProperty(i + "." + key + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is void but an @return tag is present in the comments. There should be no @return tag since the declared return type is void.");
         }
         else
         {
           if(returnTags.length==1)
-            prop.setProperty(i + "." + j + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is " + returntype + " and there is an @return tag present in the comments.");
+            prop.setProperty(i + "." + key + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is " + returntype + " and there is an @return tag present in the comments.");
           else if(returnTags.length==0) 
-            prop.setProperty(i + "." + j + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is " + returntype + " but there is no @return tag present in the comments.");
+            prop.setProperty(i + "." + key + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is " + returntype + " but there is no @return tag present in the comments.");
           else if(returnTags.length > 1)
-            prop.setProperty(i + "." + j + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is " + returntype + " but there is " + returnTags.length + " @return tags present in the comments.  There should only be one @return tag.");
+            prop.setProperty(i + "." + key + ".a", "Analyzed method " + methods[j].name() + "'s declared return type and @return tag. The declared return type is " + returntype + " but there is " + returnTags.length + " @return tags present in the comments.  There should only be one @return tag.");
         }
         
 /////////////////param tags/////////////////
@@ -100,14 +101,16 @@ public class CheckForTags
               check = true;
             if(check)
             {
-              prop.setProperty(i + "." + j + ".b" + paramCount, "Analyzed method " + methods[j].name() + "'s parameter " + parameter[s].typeName() + " " + parameter[s].name() + ". The paramter and paramter type match the @param tag in the comments.");
+			  String count = numberFormat(paramCount);
+              prop.setProperty(i + "." + key + ".b" + count, "Analyzed method " + methods[j].name() + "'s parameter " + parameter[s].typeName() + " " + parameter[s].name() + ". The paramter and paramter type match the @param tag in the comments.");
               paramCount++;
               break;
             }
           }
           if(!check)
           {
-            prop.setProperty(i + "." + j + ".b" + paramCount, "Analyzed method " + methods[j].name() + "'s parameter " + parameter[s].typeName() + " " + parameter[s].name() + ". There is no @param tag present for this paramter.");
+			String count = numberFormat(paramCount);
+            prop.setProperty(i + "." + key + ".b" + count, "Analyzed method " + methods[j].name() + "'s parameter " + parameter[s].typeName() + " " + parameter[s].name() + ". There is no @param tag present for this paramter.");
             paramCount++;
           }
         }
@@ -122,7 +125,8 @@ public class CheckForTags
           }
           if(!check)
           {
-            prop.setProperty(i + "." + j + ".b" + paramCount, "Analyzed method " + methods[j].name() + "'s @param tag " + paramTags[s].text() + ". There is no parameter in the method for the this @param tag.");
+			String count = numberFormat(paramCount);
+            prop.setProperty(i + "." + key + ".b" + count, "Analyzed method " + methods[j].name() + "'s @param tag " + paramTags[s].text() + ". There is no parameter in the method for the this @param tag.");
             paramCount++;
           }
         }
@@ -142,14 +146,16 @@ public class CheckForTags
               check = true;
             if(check)
             {
-              prop.setProperty(i + "." + j + ".c" + throwsCount, "Analyzed method " + methods[j].name() + "'s exception " + exceptions[s].name() + ". The exception matches the @throws tag in the comments.");
+			  String num = numberFormat(throwsCount);
+              prop.setProperty(i + "." + key + ".c" + num, "Analyzed method " + methods[j].name() + "'s exception " + exceptions[s].name() + ". The exception matches the @throws tag in the comments.");
               throwsCount++;
               break;
             }
           }
           if(!check)
           {
-            prop.setProperty(i + "." + j + ".c" + throwsCount, "Analyzed method " + methods[j].name() + "'s exception " + exceptions[s].name() + ". There is no @throws tag present for this exception.");
+			String num = numberFormat(throwsCount);
+            prop.setProperty(i + "." + key + ".c" + num, "Analyzed method " + methods[j].name() + "'s exception " + exceptions[s].name() + ". There is no @throws tag present for this exception.");
             throwsCount++;
           }
         }
@@ -164,12 +170,26 @@ public class CheckForTags
           }
           if(!check)
           {
-            prop.setProperty(i + "." + j + ".c" + throwsCount, "Analyzed method " + methods[j].name() + "'s @throws tag " + throwsTags[s].text() + ". There is no exception in the method for this @throws tag.");
+			String num = numberFormat(throwsCount);
+            prop.setProperty(i + "." + key + ".c" + num, "Analyzed method " + methods[j].name() + "'s @throws tag " + throwsTags[s].text() + ". There is no exception in the method for this @throws tag.");
             throwsCount++;
           }
         }
       }
     }    
     return prop;
+  }
+  
+  private String numberFormat(int value)
+  {
+	  String newValue;
+	  if(value<10)
+		  newValue = "00" + value;
+	  else if(value<100)
+		  newValue = "0" + value;
+	  else
+		  newValue = "" + value;
+	  
+	  return newValue;
   }
 }
