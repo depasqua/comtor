cb7f65d45faadccfd8ce0e356c2115e50c80a0fe
ssigwart <ssigwart>
2008-11-05 22:27:29 +0000

    No longer needed.

--- web/directory.php
+++
@@ -1,7 +0,0 @@
-<?
-  // Define private directory where config.php is
-  define("PRIVATE_DIR", "/home/sigwart4/private/");
-
-  $dir = "/home/sigwart4/www/";
-  define("UPLOAD_DIR", "/home/sigwart4/www/upload/")
-?>
