1b1e40777d6ffe6238500e9264cd0c2b0f67d7f3
James Sant'Angelo <jiennne@gmail.com>
2010-02-09 18:23:24 -0500

    Fixed Reading Level; it is now recognized and prepackaged.

--- code/src/comtor/analyzers/ReadingLevel.java
+++ code/src/comtor/analyzers/ReadingLevel.java
@@ -7,82 +7,42 @@ import java.util.*;
 import java.text.*;
 import java.io.*;
 
+//Changes
+
 public class ReadingLevel implements ComtorDoclet
 {
 	private Properties prop = new Properties();
-
-	public Properties analyze(RootDoc root)
-	{
-	    prop.setProperty("title", "Reading Level"); //doclet title
-	    try
-	    {
 	
-		String dir = System.getProperty("user.dir");
-		System.out.println("User directory: " + dir);
-		BufferedReader fileNames =  new BufferedReader(new FileReader(dir+"/source.txt"));
+	public ReadingLevel(){
+	};
+
+	public Properties analyze(RootDoc rootDoc)
+	{
+	  //Doclet Title
+	  prop.setProperty("title", "Reading Level");
+	
 		// Initializing variables for reading files
 		String line;
 		int next;
 		String allFiles = "";
-		// Read in all files
-		while((line = fileNames.readLine()) != null)
-		{
-			BufferedReader br = new BufferedReader(new FileReader(line));
-			next = br.read();
-			while(next != -1)
-			{
-				allFiles = allFiles + (char)next;
-				next = br.read();
-			}
-		}
-		// Create a pattern to match comments and run matches
-		Pattern ml = Pattern.compile("//.*|(s?)/[*][^([*]/)]*[*]/");
-		Matcher match = ml.matcher(allFiles);
-		// Pull out all comments
-		String matched = "";
-		char temp;
 		String allComments = "";
-	    	while(match.find())
-		{
-			matched = match.group();
-			// Clean up comments found.
-			// Multiline, trim off '*/' at the end of the comment
-			if(matched.charAt(1)=='*') 
-				matched = matched.substring(0,matched.length()-2);
-
-			// Trim off '//' or '/*' from front of comment
-			matched = matched.substring(2);
-			// Eliminate trailing and leading whitespace
-			matched = matched.trim();
-
-			/* Makes comment a complete sentence for the reading algorithm;
-			   later sentence detection algorithms will be used.
-			   This is a hack for now. */	
-			if(matched.length() > 0)
-			{
-				temp = matched.charAt(matched.length()-1);
-				if(temp == '.' || temp == '?' || temp == '!'); // Do nothing
-				else	
-				matched = matched + ". ";
-				// Removes any newlines and tabs from the string
-
-				matched = matched.replace('\n',' ');
-				matched = matched.replace('\t','\0');
-
-				// Comment added to allComments to be analyzed for reading level.  
-				allComments +=matched;
-			}
-		}
+		
+		ClassDoc[] classes = rootDoc.classes();
+    	for(int i = 0; i < classes.length; i++)
+      		allComments += processClass(classes[i]);
+			
 		// 1) Count sentences
 		// 2) Count words
 		// 3) Count syllables
 		int numSentences = countSentences(allComments);
 		int numWords = countWords(allComments);
 		int numSyllables = countSyllables(allComments);
-		//Flesch-Kincaid Grade Level = (0.39* Average Sentence Length)+(11.8*Average Syllables per Word)-15.59
+		//Flesch-Kincaid Grade Level = 
+			//(0.39* Average Sentence Length)+(11.8*Average Syllables per Word)-15.59
 		double avgSentenceLength = (double)numWords/numSentences;
 		double avgSyllablesPerWord = (double)numSyllables/numWords;
 		double FleschKincaid = (0.39*avgSentenceLength)+(11.8*avgSyllablesPerWord)-15.59;
+		
 		System.out.println("AverageSentenceLength: " + avgSentenceLength);	
 		System.out.println("AverageSyllablesPerWord: " + avgSyllablesPerWord);
 		System.out.println("Total sentences: " + numSentences);
@@ -92,21 +52,87 @@ public class ReadingLevel implements ComtorDoclet
 	
 		DecimalFormat fmt = new DecimalFormat("#0.00");
 
-		prop.setProperty("000.000.000", "AverageSentanceLength: " + fmt.format(avgSentenceLength));
-		prop.setProperty("000.000.001", "AverageSyllablesPerWord: " + fmt.format(avgSyllablesPerWord));
-		prop.setProperty("000.000.002", "Total sentences: " + fmt.format(numSentences));
-		prop.setProperty("000.000.003", "Total words: " + fmt.format(numWords));
-		prop.setProperty("000.000.004", "Total syllables: " + fmt.format(numSyllables));
-		prop.setProperty("000.000.005", "Flesch-Kincaid Grade Level = " + fmt.format(FleschKincaid));
+		prop.setProperty("000.000.000", "Average sentence length: " + fmt.format(avgSentenceLength));
+		prop.setProperty("000.000.001", "Average syllables per word: " + fmt.format(avgSyllablesPerWord));
+		prop.setProperty("000.000.002", "Total number of sentences: " + fmt.format(numSentences));
+		prop.setProperty("000.000.003", "Total number of words: " + fmt.format(numWords));
+		prop.setProperty("000.000.004", "Total number of syllables: " + fmt.format(numSyllables));
+		prop.setProperty("000.000.005", "Flesch-Kincaid Grade Level: " + fmt.format(FleschKincaid));
 		prop.setProperty("score", "" + getGrade());
-	    }
-	    catch(Exception e)
-	    {
-		System.out.println(e);
-	    }
 
 	    return prop;	
 	}
+	
+	/*************************************************************************
+  * Processes a single class
+  *
+  * @param class Class to process
+  * @return String String to which all comments will be written/saved
+  *************************************************************************/
+  private String processClass(ClassDoc classDoc)
+  {
+  	String allComments = "";
+
+    // Add class comment
+    allComments = parseComment(classDoc.commentText());
+
+    // Get all fields
+    FieldDoc[] fields = classDoc.fields();
+    for(int h = 0; h < fields.length; h++)
+    {
+      // Add field comment
+      allComments += parseComment(fields[h].commentText());
+    }
+
+    // Get inner classes
+    ClassDoc[] inner_classes = classDoc.innerClasses();
+    for(int h = 0; h < inner_classes.length; h++)
+      processClass(inner_classes[h]);
+
+    // Get comments for tags
+    Tag[] tags = classDoc.tags();
+    for (int h = 0; h < tags.length; h++)
+      allComments += parseComment(tags[h].text());
+
+    // Get comments for each method
+    MethodDoc[] methods = classDoc.methods();
+    for(int j = 0; j < methods.length; j++)
+    {
+      // Get comments for tags
+      tags = methods[j].tags();
+      for (int h = 0; h < tags.length; h++)
+        allComments += parseComment(tags[h].text());
+
+      // Get method comments
+      allComments += parseComment(methods[j].commentText());
+    }
+    
+    return allComments;
+  }
+
+  /*************************************************************************
+  * Parses words out of comments and removes punctuation
+  *
+  * @param comment Comment to parse
+  * @return String String to which all comments are added after processing
+  *************************************************************************/
+  private String parseComment(String comment)
+  {
+	char temp;
+    // Replace parenthesis, brackets, dashes, and periods with spaces
+    comment = comment.replaceAll("[()<>-]|\\+"," ");
+
+	//Creates sentences out of comments to be analyzed.
+	if (comment.length() > 0){
+		temp = comment.charAt(comment.length() - 1);
+    		if (temp == '.' || temp == '?' || temp == '!');
+   		else
+    			comment += '.';
+    	}
+
+	// Comment added to allComments to be analyzed for reading level.  
+	return comment;
+  }
 
 	// At present it simply counts the number of '.','?', and '!' characters to estimate number of sentences.
 	public static int countWords(String text)
