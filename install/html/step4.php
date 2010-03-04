885c9e0d9887c4bd2c8b9b353437dde485a83339
James Sant'Angelo <jiennne@gmail.com>
2010-03-04 15:42:21 -0500

    Cleaned up install process, added link on finish.

--- install/html/step4.php
+++ install/html/step4.php
@@ -5,12 +5,6 @@
 <p>
 After submitting, error messages may appear indicating that the permissions are not correct on some files and directories.  If any of these files or directories do not exist, please create them.  Then change the permissions on the files so that the web server can read and write to them.  After this step is over, you may change the permissions to read only on the configuration files.  However, you may not change the permissions on the "uploads" or "template_c" directories.
 </p>
-<p>
-After this step is completed, you may wish to change the permissions on certain files back to what they were originally. To "relock" these files, please open a command line, navigate to the install directory, and type the following:
-</p>
-<code>
-bash permissions.sh lock
-</code>
 
 <form name="form" method="post" action="">
 <div>
--- install/html/step8.php
+++ install/html/step8.php
@@ -1,7 +1,16 @@
 <h1>Setup Complete</h1>
 
-<?php require_once("error_box.php"); ?>
+<?php
+require_once("error_box.php");
+require_once("../comtor_data/config/config.php");
+?>
 
 <p>
 Setup is now complete.  You may test out the system.
 </p>
+<p>Please remember, the default password set for the administrative account you just created is 'comtor' (no quotes). This can be changed once you log in for the first time.
+</p>
+<a href="<?php echo $dir ?>">"Let's Go!"</a>
+
+
+
