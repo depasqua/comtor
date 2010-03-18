cf8fa11011081c3da0bf032459aa01f6d44908d5
James Sant'Angelo <jiennne@gmail.com>
2010-03-18 17:30:02 -0400

    Introduced new permissions script for new installation process.

---
+++ permissions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+#######################################################################
+#                                                                     #
+#Note on usage:                                                       #
+#                                                                     #
+#This script will unlock certain files (change permissions for the web#
+#installer to write).                                                 #
+#                                                                     #
+#In order to run this script, it is probably necessary to have root   #
+#access.                                                              #
+#                                                                     #
+#                                                                     #
+#To run this file, use the command "bash packager.sh".		      #
+#                                                                     #
+#######################################################################
+
+echo "Unlocking..."
+
+#unlock /comtor_data/uploads
+cd comtor_data
+chmod go+w uploads
+cd ..
+
+#unlock /www/templates_c
+cd www
+chmod go+w templates_c
+
+echo "Done."
+
