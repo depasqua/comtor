d20aa27a05d2476014400ddb8abcc94463613d40
Peter DePasquale <pjdepasq@users.sourceforge.net>
2011-08-13 20:17:23 -0400

    Removed unneeded import, moved common code to Util class (new) and corrected method calls to moved code as necessary.

--- code/src/comtor/analyzers/SpellCheck.java
+++ code/src/comtor/analyzers/SpellCheck.java
@@ -22,7 +22,6 @@ import com.sun.javadoc.*;
 import java.io.*;
 import java.net.*;
 import java.util.*;
-import java.util.regex.*;
 
 /**
  * The SpellCheck class is a tool to check spelling in comments. It is capable of determining
@@ -84,8 +83,8 @@ public final class SpellCheck implements ComtorDoclet {
 		int num = 0;
 		
 		// Load the dictionary and java class listing (both sets contain valid 'words')	
-		loadDataList("dictionary.txt", dictionary);
-		loadDataList("javaclasslist.txt", javaClassNamesList);
+		Util.loadDataList("dictionary.txt", dictionary);
+		Util.loadDataList("javaclasslist.txt", javaClassNamesList);
 		System.err.println("right in dict: " + validWord("right"));
 
 		//-------------------------------------//
@@ -133,117 +132,6 @@ public final class SpellCheck implements ComtorDoclet {
 		// Set the score for this analysis and return the property list (report)
 		prop.setProperty("score", "" + getGrade());
 		return prop;
-	}
-
-	/**
-	 * Attempts to load the local and web-based versions of data lists. This method is used to load
-	 * both dictionary files and java class lists into HashSets for use in the spell checker.
-	 * Whenever possible, the local copy will be updated if its internal version number is less than
-	 * the web-based version number.
-	 *
-	 * @param fileName the base of the filename to attempt to load
-	 * @param dataList a reference to a hash set of strings which will hold the data list
-	 */
-	private void loadDataList(String fileName, HashSet<String> dataList) {
-		if (dataList == null)
-			dataList = new HashSet<String> ();
-			
-		// Attempt to load/fetch the most recent dictionary list.
-		String netFileName = "http://www.comtor.org/" + fileName;
-		String localFileName = System.getProperty("user.dir") +
-							   System.getProperty("file.separator") + fileName;
-		Scanner netScanner = null;
-		String netFileVersion = null;
-		Scanner localScanner = null;
-		String localFileVersion = null;
-		
-		try {
-			// Attempt to open / read most recent list on web
-			netScanner = new Scanner((new URL(netFileName)).openStream());			
-			netFileVersion = netScanner.nextLine();
-		} catch (MalformedURLException mue) {
-			System.err.println(mue);  // Bad URL, it's ok, default to local copy if possible.
-		} catch (IOException ioe) {
-			System.err.println(ioe); // No access to the 'net, default to local copy if possible.
-		}
-
-		try {
-			// Attempt to open / read local copy
-			File localFile = new File (localFileName);
-			if (localFile.exists()) {
-				localScanner = new Scanner(localFile);
-				localFileVersion = localScanner.nextLine();
-			}
-		} catch (IOException ioe) {
-			System.err.println(ioe);
-		}
-		
-		// Determine what version(s) we have, rewrite local version if needed
-		if (localFileVersion == null && netFileVersion != null) {
-			// Write a new local copy and use the 'net version
-			try {
-				PrintStream localFile = new PrintStream (new File(localFileName));
-				localFile.println(netFileVersion);
-				while (netScanner.hasNextLine()) {
-					String line = netScanner.nextLine();
-					localFile.println(line);
-					dataList.add(line.toLowerCase());
-				}
-				localFile.close();
-			} catch (FileNotFoundException fnfe) {
-				System.err.println(fnfe);
-				System.exit(1);
-			}
-		} else if (localFileVersion == null && netFileVersion == null) {
-			// Well, there's not much else we can do...
-			System.exit(1);
-		} else if (localFileVersion != null && netFileVersion != null) {
-			// Check for freshness of local version
-			if (localFileVersion.compareTo(netFileVersion) < 0) {
-				// Update local copy, net copy is fresher
-				try {
-					PrintStream localFile = new PrintStream (new File(localFileName));
-					localFile.println(netFileVersion);
-					while (netScanner.hasNextLine()) {
-						String line = netScanner.nextLine();
-						localFile.println(line);
-						dataList.add(line.toLowerCase());
-					}
-					localFile.close();
-				} catch (FileNotFoundException fnfe) {
-					System.err.println(fnfe);
-					System.exit(1);
-				}
-			} else
-				// Load the local version, no update was necessary
-				loadFileToList(localFileName, dataList);
-		} else
-			// Use the local version, 'net is unavailable.
-			loadFileToList(localFileName, dataList);
-	}
-	
-	/**
-	 * Attempts to load the specified local file into the specified hash set. The input assumes the
-	 * first line is a version number and it is disregarded for this use.
-	 *
-	 * @param localFileName the name of the local file to read
-	 * @param dataList a reference to the hash set into which the file contents are placed
-	 */
-	private void loadFileToList(String localFileName, HashSet<String> dataList) {
-		try {
-			Scanner scan = null;
-			File localFile = new File (localFileName);
-			if (localFile.exists()) {
-				scan = new Scanner(localFile);
-				scan.nextLine(); // drop the version number, we don't care right here
-			}
-			while (scan.hasNextLine())
-				dataList.add(scan.nextLine().toLowerCase());
-	
-			scan.close();
-		} catch (IOException ioe) {
-			System.err.println(ioe);
-		}
 	}
 
 	/**
