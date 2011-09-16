afd18a8e400faaec5299144313533a39d0479035
Peter DePasquale <pjdepasq@users.sourceforge.net>
2011-09-16 18:42:23 -0400

    Enahnced parsing, provides grading result, enhanced metrics in report. Fixes reporting bug that associated package-level comments with a class.

--- code/src/comtor/analyzers/OffensiveWords.java
+++ code/src/comtor/analyzers/OffensiveWords.java
@@ -19,6 +19,7 @@ package comtor.analyzers;
 
 import com.sun.javadoc.*;
 import java.util.*;
+import java.text.*;
 
 /**
  * The BadWords class is a tool to check for offensive words in comments.
@@ -29,8 +30,18 @@ public final class OffensiveWords implements ComtorDoclet {
 	// The analysis report (property list) returned by this doclet
 	private Properties prop = new Properties();
 
+	// A hash set that will hold the list of 'bad' (offensive) words found in comments
 	private HashSet<String> badWordsList = new HashSet<String>();
 	
+	// A counter for the classes, used in the properties list
+	private int classID = 0;
+	
+	// Formatter for the report
+	private DecimalFormat formatter = new DecimalFormat("##0000.000");
+	
+	// Counter for the number of offensive words
+	private int numBadWords = 0;
+
 	/**
 	 * Examine each class and its methods. Calculate the length of each method's comments.
 	 *
@@ -38,27 +49,43 @@ public final class OffensiveWords implements ComtorDoclet {
 	 * @return Properties list
 	 */
 	public Properties analyze(RootDoc rootDoc) {
-		prop.setProperty("title", "Spell Checker");
+		prop.setProperty("title", "Offensive Words");
 
 		// Load the bad words list into a hashset
 		Util.loadDataList("badwords.txt", badWordsList);
 		
-		// Find bad words in the RootDoc's comments
-		parseComment(rootDoc.commentText());
+		// Capture the starting time, just prior to the start of the analysis
+		long startTime = new Date().getTime();
 
 		// Find bad words in the RootDoc's packages
-		for (PackageDoc packageDoc : rootDoc.specifiedPackages())
-			parseComment(packageDoc.commentText());
+		for (PackageDoc packageDoc : rootDoc.specifiedPackages()) {
+			prop.setProperty(formatter.format(classID), "Package: " + packageDoc.name());
+			parseComment(packageDoc.commentText(), 0.000f);
+			classID++;
+		}
 		
 		// Parse the comments for each class found in the root doc
 		for (ClassDoc classMember : rootDoc.classes()) {
+			prop.setProperty(formatter.format(classID), "Class: " + classMember.qualifiedName());
 			parseClass(classMember);
+			classID++;
 			
 			// Parse the comments for any/all inner classes of any class
-			for (ClassDoc innerClass : classMember.innerClasses())
+			for (ClassDoc innerClass : classMember.innerClasses()) {
+				prop.setProperty(formatter.format(classID), "Class: " + classMember.qualifiedName());
 				parseClass(innerClass);
+				classID++;
+			}
 		}
 
+		// Capture the ending time, just after the termination of the analysis
+		long endTime = new Date().getTime();
+
+		prop.setProperty("score", "" + getGrade());
+		prop.setProperty("start time", Long.toString(startTime));
+		prop.setProperty("end time", Long.toString(endTime));
+		prop.setProperty("execution time", Long.toString(endTime - startTime));
+		prop.setProperty("metric1", "Number of offensive words found: " + numBadWords);
 		return prop;
 	}
 	
@@ -68,45 +95,56 @@ public final class OffensiveWords implements ComtorDoclet {
 	 * @pararm classMember a reference to the class (as a ClassDoc object) to parse
 	 */
 	private void parseClass(ClassDoc classMember) {
-		parseComment(classMember.commentText());
+		float memberNum = 0.000f;
+		parseComment(classMember.commentText(), memberNum);
 		
 		// Parse the comment for each constructor
 		for (ExecutableMemberDoc memberDoc : classMember.constructors()) {
-			parseComment(memberDoc.commentText());
+			memberNum += 0.001f;
+			prop.setProperty(formatter.format(classID+memberNum), "\tConstructor: " + memberDoc.qualifiedName());
+			parseComment(memberDoc.commentText(), memberNum);
 
 			// Parse the parameter comments
 			for (ParamTag param : memberDoc.paramTags()) {
-				parseComment(param.parameterComment());
+				memberNum += 0.001f;
+				parseComment(param.parameterComment(), memberNum);
 			}
 			
 			// Parse the parameter comments
 			for (ParamTag param : memberDoc.typeParamTags()) {
-				parseComment(param.parameterComment());
+				memberNum += 0.001f;
+				parseComment(param.parameterComment(), memberNum);
 			}
 			
 			// Parse the throws comments
 			for (ThrowsTag tag : memberDoc.throwsTags()) {
-				parseComment(tag.exceptionComment());
+				memberNum += 0.001f;
+				parseComment(tag.exceptionComment(), memberNum);
 			}
 		}
 		
 		// Parse the comment for each method
 		for (ExecutableMemberDoc memberDoc : classMember.methods()) {
-			parseComment(memberDoc.commentText());
+			memberNum += 0.001f;
+			prop.setProperty(formatter.format(classID+memberNum), "\tMethod: " + memberDoc.qualifiedName());
+			parseComment(memberDoc.commentText(), memberNum);
 			
 			// Parse the parameter comments
 			for (ParamTag param : memberDoc.paramTags()) {
-				parseComment(param.parameterComment());
+//				memberNum += 0.001f;
+				parseComment(param.parameterComment(), memberNum);
 			}
 			
 			// Parse the parameter comments
 			for (ParamTag param : memberDoc.typeParamTags()) {
-				parseComment(param.parameterComment());
+//				memberNum += 0.001f;
+				parseComment(param.parameterComment(), memberNum);
 			}
 			
 			// Parse the throws comments
 			for (ThrowsTag tag : memberDoc.throwsTags()) {
-				parseComment(tag.exceptionComment());
+//				memberNum += 0.001f;
+				parseComment(tag.exceptionComment(), memberNum);
 			}
 			
 			// Needs to also fetch @return tag and extract comment (not trivial, see API)
@@ -114,12 +152,16 @@ public final class OffensiveWords implements ComtorDoclet {
 		
 		// Parse the comment for each field
 		for (FieldDoc field : classMember.fields()) {
-			parseComment(field.commentText());
+			memberNum += 0.001f;
+			prop.setProperty(formatter.format(classID+memberNum), "\tField: " + field.qualifiedName());
+			parseComment(field.commentText(), memberNum);
 		}
 		
 		// Parse the comment for each enum
 		for (FieldDoc field : classMember.enumConstants()) {
-			parseComment(field.commentText());
+			memberNum += 0.001f;
+			prop.setProperty(formatter.format(classID+memberNum), "\tEnum: " + field.qualifiedName());
+			parseComment(field.commentText(), memberNum);
 		}
 	}
 
@@ -129,7 +171,11 @@ public final class OffensiveWords implements ComtorDoclet {
 	 *
 	 * @param comment The comment string which we wish to parse
 	 */
-	private void parseComment(String comment) {
+	private void parseComment(String comment, float memberID) {
+		String fmtID = formatter.format(((float) classID) + memberID);
+		DecimalFormat shortFormatter = new DecimalFormat("000");
+		int itemID = 0;
+
 		// Replace parenthesis, brackets, dashes, and periods with spaces
 		comment = comment.replaceAll("[\\p{Punct}&&[^']]"," ");
 
@@ -137,8 +183,11 @@ public final class OffensiveWords implements ComtorDoclet {
 		Scanner scan = new Scanner(comment);
    		while (scan.hasNext()) {
 			String word = scan.next().toLowerCase();
-			if (badWordsList.contains(word))
-				System.out.println("Bad word: " + word);
+			if (badWordsList.contains(word)) {
+				String wordID = fmtID + '.' + shortFormatter.format(itemID++);
+				numBadWords++;
+				prop.setProperty(wordID, word);
+			}
 		}
 	}			
 	
@@ -159,7 +208,10 @@ public final class OffensiveWords implements ComtorDoclet {
 	 * from comments to the total number of works in the comments.
 	 */
 	public float getGrade() {
-		return -1.0f;
+		if (numBadWords != 0)
+			return 100.0f;
+		else
+			return 0.0f;
 	}
 
 	/**
