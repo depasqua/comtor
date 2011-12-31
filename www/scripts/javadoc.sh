21226fe8ee6df6553f3ccaab000ffccd01157894
Peter DePasquale <pjdepasq@users.sourceforge.net>
2011-12-31 00:21:56 -0800

    Tighten up the script commands, use new doclet listing file, added comments.

--- www/scripts/javadoc.sh
+++ www/scripts/javadoc.sh
@@ -1,88 +1,68 @@
 #!/bin/sh
-#set -x
+#
+# Parameters to this script:
+# $1: temporary directory, previously created by run.php, where processing will be handled
+# $2: name of the uploaded jar file
+# $3: user id
+# $4: assignment id
+#
+set -u
 
 # Script expects to be run from www folder
 
 # Load config info
 . scripts/config.sh
-
 export set CLASSPATH=$CLASSPATH
 
-#create temp dir based on tempFileName
-cd $UPLOAD_PATH
-#mkdir $1
-chmod 755 $1
-cd $1
+# change the permissions on the temporary directory ($1) and doclet list file ($2)
+chmod 755 $UPLOAD_PATH/$1
+cd $UPLOAD_PATH/$1
 
-#create src dir for all source files to be stored within the temp dir
-mkdir src
-chmod 755 src
-
-#move jar file from upload dir to src
-mv $2 src
-
-#move text file (list of doclets) to the temp dir and rename it to Doclets.txt
-cd $UPLOAD_PATH
-mv $1.txt $1
-cd $1
-cp $1.txt docletList.properties
-rm $1.txt
-
-#unjar the jar file in src dir and set the permissions
-cd src
+# unjar the jar file in temp dir and set the permissions
 jar xf $2
-cd ..
-chmod 755 -R src
 
-#move the jar file from the src dir back to the temp dir
-cd src
-mv $2 ../$2
-
-#find the list of java files within the src dir and store the list in source.txt
-cd $UPLOAD_PATH/$1/
-find src -name *.java > source.txt
-chmod 755 *.txt
+# find the list of java files within the src dir and store the list in source.txt
+find . -name *.java > source.txt
 
 # Check that there is source code
-if [ ! -s "./source.txt" ]; then
+if [ ! -s "source.txt" ]; then
   # Delete the temporary folder
   rm -r $UPLOAD_PATH/$1
   exit 3
 fi
 
-#compile and list class files
-cd src
-javac `find . | grep .*\.java$` > ../CompileOut.txt 2>&1
-# Get return value of javac
+# compile and list class files
+javac `find . | grep .*\.java$` > ../compileOut.txt 2>&1
+
+# obtain return value of javac
 compiled=$?
-find . | grep .*\.class$ > classes.txt
-cd ..
 
-#take the contents of source.txt and store it in MYVAR
-MYVAR=`cat source.txt`
+# obtain the contents of source.txt and store it in MYVAR
+sourceFiles=`cat source.txt`
 
-#make text file with userId
-cat >> userId.txt << EOF
+# make text file with userId
+cat >> $UPLOAD_PATH/$1/userId.txt << EOF
 $3
 EOF
 
 # Run javadoc if code compiled
 if [ $compiled = 0 ]; then
-  javadoc -private --assignment-id $4 --config-file $JAVA_CONFIG -doclet comtor.ComtorDriver -docletpath $CLASSES $MYVAR > JavadocOut.txt 2>&1
+  javadoc -private --assignment-id $4 --config-file $JAVA_CONFIG -doclet comtor.ComtorDriver -docletpath $CLASSES $sourceFiles > javadocOut.txt 2>&1
+  echo "javadoc -private --config-file $JAVA_CONFIG -doclet comtor.ComtorDriver -docletpath $CLASSES $sourceFiles" > $UPLOAD_PATH/$1/debug.txt
   javadocRtn=$?
 else
   java -cp $CLASSES:$CLASSPATH comtor.GenerateErrorReport $JAVA_CONFIG
 fi
 
 if [ $javadocRtn != 0 ]; then
-exit 2
+	exit 2
 fi
 
 # Delete the temporary folder
-rm -r $UPLOAD_PATH/$1
+#rm -r $UPLOAD_PATH/$1
 
 if [ $compiled != 0 ]; then
-exit 1
+	exit 1
 fi
 
 exit 0
