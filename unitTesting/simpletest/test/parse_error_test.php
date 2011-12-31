8af416d471e9eb07bb618b8d5090c9d7eea09549
Peter DePasquale <pjdepasq@users.sourceforge.net>
2011-12-31 10:37:28 -0800

    Removing old, unneeded code.

--- unitTesting/simpletest/test/parse_error_test.php
+++
@@ -1,9 +0,0 @@
-<?php
-// $Id: parse_error_test.php,v 1.1 2009-04-28 17:58:30 ssigwart Exp $
-require_once('../unit_tester.php');
-require_once('../reporter.php');
-
-$test = &new TestSuite('This should fail');
-$test->addFile('test_with_parse_error.php');
-$test->run(new HtmlReporter());
-?>
--- unitTesting/simpletest/test/test_with_parse_error.php
+++
@@ -1,8 +0,0 @@
-<?php
-    // $Id: test_with_parse_error.php,v 1.1 2009-04-28 17:58:30 ssigwart Exp $
-    
-    class TestCaseWithParseError extends UnitTestCase {
-        wibble
-    }
-    
-?>
