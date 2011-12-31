e723d802bb3aab5d89e6c5f3c22f2043135a0335
Peter DePasquale <pjdepasq@users.sourceforge.net>
2011-12-31 00:14:09 -0800

    Cleaned up the comments and defined a COMTOR_DATA variable for easier path editing.

--- www/scripts/config.sh
+++ www/scripts/config.sh
@@ -1,25 +1,29 @@
 #!/bin/sh
+# Define the directory of the COMTOR data area (where uploads are processed)
+COMTOR_DATA=/mywebsite/comtor/comtor_data
 
-#dir of upload path
-UPLOAD_PATH=/mywebsite/comtor/comtor_data/uploads
+# Define the directory of the COMTOR uploads
+UPLOAD_PATH=$COMTOR_DATA/uploads
 
-#dir of class files for javadoc
-CLASSES=/mywebsite/comtor/comtor_data/code/comtor.jar
+# Define the directory of the class files for comtor
+CLASSES=$COMTOR_DATA/code/comtor.jar
 
-#classpath used to find all dependencies
-#this classpath must include the locations of the following five files:
+# Define the CLASSPATH used to find all dependencies
+# this CLASSPATH must include the locations of the following five files:
 #	/comtor_data/code/stringtemplate-3.2.jar
 #	/comtor_data/code/comtor.jar
 #	/comtor_data/code/antlr-3.1.1.jar
 #	/comtor_data/code/mysql-connector-java-5.1.6-bin.jar
 #	/comtor_data/code/antlr-3.1.1-runtime.jar
-#See below for an example.
-CLASSPATH='.:/mywebsite/comtor/comtor_data/code/stringtemplate-3.2.jar:/mywebsite/comtor/comtor_data/code/comtor.jar:/mywebsite/comtor/comtor_data/code/antlr-3.1.1.jar:/mywebsite/comtor/comtor_data/code/mysql-connector-java-5.1.6-bin.jar:/mywebsite/comtor/comtor_data/code/antlr-3.1.1-runtime.jar'
+# See below for an example.
+CLASSPATH='.:$COMTOR_DATA/code/stringtemplate-3.2.jar:$COMTOR_DATA/code/comtor.jar:$COMTOR_DATA/code/antlr-3.1.1.jar:$COMTOR_DATA/code/mysql-connector-java-5.1.6-bin.jar:$COMTOR_DATA/code/antlr-3.1.1-runtime.jar'
 
-#dir of the java.properties file
-JAVA_CONFIG=/mywebsite/comtor/comtor_data/config/java.properties
 
-#where all of the following commands can be found
+# Define the directory of the java.properties file
+JAVA_CONFIG=$COMTOR_DATA/config/java.properties
+
+
+# where all of the following commands can be found
 alias java='/usr/bin/java'
 alias javac='/usr/bin/javac'
 alias javadoc='/usr/bin/javadoc'
@@ -31,6 +35,3 @@ alias rm='/bin/rm'
 alias find='/usr/bin/find'
 alias cat='/bin/cat'
 alias grep='/bin/grep'
- 
-
-?>
