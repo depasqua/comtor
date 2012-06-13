59c241a5fa8b70f8cbebf729d0d49190ca4f65fd
Michael E. Locasto <michael@freshdefense.net>
2012-06-13 15:25:34 -0600

    MEL: minor changes

--- bin/crowdt.sh
+++ bin/crowdt.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 CROWDT_HOME=../code
-CLIENT=org.comtor.cloud.crowd.FileClient
+#CLIENT=org.comtor.cloud.crowd.FileClient
+CLIENT=org.comtor.cloud.crowd.JDocClient
 CONFIG_FILE=src/org/comtor/cloud/crowd/crowd.properties
 
 java -cp "$CROWDT_HOME"/classes/ "$CLIENT" "$CROWDT_HOME/$CONFIG_FILE"
